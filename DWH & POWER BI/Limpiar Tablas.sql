560995a032a417c664f8027ab809a27443bacd14
Fernando Madrid <fernando.madrid21@gmail.com>
2022-09-05 17:55:24 -0300

    Agregar archivos de proyecto.

---
+++ DWH & POWER BI/Limpiar Tablas.sql
@@ -0,0 +1,9 @@
+delete from Atenciones
+delete from DimCategoria
+delete from DimDetalle
+delete from DimTipo
+
+delete from STAGING.dbo.BaseAtenciones
+delete from STAGING.dbo.BaseTickets
+
+--TRUNCATE TABLE [dbo].[DimTipo]
